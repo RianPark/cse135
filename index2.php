0b46959360b30176d21fb9bae5213f1d9d44b099
RianPark <piano11keyy@gmail.com>
2016-08-09 09:00:08 -0700

    ?

--- index2.php
+++ index2.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>php test page</title>
-	</head>
-	<body>
-		<p>
-			<?php
-				echo "For test php!";
-			?>
-		</p>
-	</body>
-</html>
+<?php
+	phpinfo();
+?>
