e24b103d4b112fca44a515cb625d5b8389b068fd
Jong Hwi Park <Jong Hwi Park>
2016-08-16 07:58:38 +0900

    2nd

--- test.js
+++ test.js
@@ -1,22 +1,35 @@
-var http = require("http");
-var color;
+var express = require('express');
+var app = express();
+var path = require('path');
+var num = 0;
 
-function random (0, 2) {
-    num =  Math.random() * 2 ;
+function randomInt( 0, 2 ){
+	num = Math.floor(Math.random() * ( 2-0 ) + 0 );
 }
 
-http.createServer(function (request, response) {
+app.get( function (request, response) {
 
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   //response.writeHead(200, {'Content-Type': 'text/plain'});
+   //var bgblue = require('ansi-bgblue');
 
-   var bgblue = require('ansi-bgblue');
- 
-   console.log(colors.bgblue());
-   response.end('Hello World JS\n');
-}).listen(8000);
+   	if( num == 0)
+	{
+   		res.sendFile(path.join(__dirname + 'blueJS.html'));
+	}
+	else if(num == 1)
+	{
+   		res.sendFile(path.join(__dirname + 'whiteJS.html'));
+	}
+	else
+	{
+   		res.sendFile(path.join(__dirname + 'redJS.html'));
+	}
+
+})
+app.listen(8000);
 
 // Console will print the message
 console.log('Hello World running at http://138.68.30.229:8000/');
