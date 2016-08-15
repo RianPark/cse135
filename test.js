0d7c9fa5fc24c644d23e2aa8a56872317dd8ec46
Jong Hwi Park <Jong Hwi Park>
2016-08-16 07:10:53 +0900

    2nd

--- test.js
+++ test.js
@@ -1,12 +1,31 @@
 var http = require("http");
+var num = 0;
+var color;
 
-http.createServer(function (request, response) {
+function random (0, 2) {
+    num =  Math.random() * 2 ;
+}
+eateServer(function (request, response) {
 
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+
+   if( num == 0 )
+   {
+		var bgred = require('ansi-bgred');
+   }
+   else if( num == 1 )
+   {
+		var bgwhite = require('ansi-bgwhite');
+   }   
+   else
+   {
+		var bgblue = require('ansi-bgblue');
+   }
+
+   response.background()
    // Send the response body as "Hello World"
    response.end('Hello World JS\n');
 }).listen(8000);
