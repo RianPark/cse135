2dfcb59efcb7c1a7409fdc93d78b3419c4e04a51
Jong Hwi Park <Jong Hwi Park>
2016-08-16 06:06:54 +0900

    2nd

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var app = express();
+var http = require("http");
 
-app.get('/', function (req, res) {
-	res.send('Hello World');
-});
+http.createServer(function (request, response) {
 
-app.listen(3000, function () {
-	console.log('Example app port 3000!');
-});
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://138.68.30.229/:8081/');
