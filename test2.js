df9cf9e0ca63cf2cdaed059ceb19ad9e69a957b1
Jong Hwi Park <Jong Hwi Park>
2016-08-16 08:28:17 +0900

    2nd

--- test2.js
+++ test2.js
@@ -2,12 +2,29 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+var num;
+var low = 1;
+var high = 3;
+
+function randomInt ( low, high ) {
+	num = Math.floor(Math.random() * ( high -low ) + low );
+}
+
 app.listen(3303, function() {
 	console.log("start");
 })
 
 app.get('/', function( req, res) {
-	fs.readFile('redJS.html', function(error, data) {
+
+	if( num == 1){
+		fs.readFile('redJS.html', function(error, data) 
+	}
+	elseif( num == 2 ){
+		fs.readFile('blueJS.html', function(error, data)
+	}
+	else{
+		fs.readFile('whiteJS.html', function(error, data)
+	}
 	res.end(data)
 	});
 });
