51f0d1d5ca29b7211c2aee23067fb237a4f5d8b0
Jong Hwi Park <Jong Hwi Park>
2016-08-16 11:31:35 +0900

    2nd

--- echoGetPHP.php
+++ echoGetPHP.php
@@ -3,13 +3,12 @@
 <?php
 header('Content-Type: text/html');
 echo "<html><head><title>Form Echo</title>";
-$color=$_GET["cSelect"];
+$color=$_GET["color_select"];
 echo "</head><body bgcolor=$color><h1>";
 $first=$_GET["firstname"];
 $last=$_GET["lastname"];
-$dtime= date("Y-m-d", time());
-$theTime= time();
-echo "Welcome $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
+
+echo "Hello $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
 echo "</h1>";
 echo"</body></html>";
 ?>
