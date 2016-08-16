7bc670aa3695b0737613a7933f9df893fc07b49a
Jong Hwi Park <Jong Hwi Park>
2016-08-16 11:15:30 +0900

    2nd

--- echoGetPHP.php
+++ echoGetPHP.php
@@ -1,13 +1,13 @@
 <?php
-header('Content-Type: text/html');
-echo "<html><head><title>Form Echo</title>";
-$color=$_GET["cSelect"];
-echo "</head><body bgcolor=$color><h1>";
-$first=$_GET["firstname"];
-$last=$_GET["lastname"];
-$dtime= date("Y-m-d", time());
-$theTime= time();
-echo "Welcome $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
-echo "</h1>";
-echo"</body></html>";
+	header('Content-Type: text/html');
+	echo "<html><head><title>Form Echo</title>";
+	$color=$_GET["color"];
+	echo "</head><body bgcolor=$color><h1>";
+	$first=$_GET["firstname"];
+	$last=$_GET["lastname"];
+	$dtime= date("Y-m-d", time());
+	$theTime= time();
+	echo "<p>Hello $first $last from a Web app written in PHP on </P>";
+	echo "<p> .date("Y-m-d h:i:sa")";
+	echo "</h1></p></body></html>";
 ?>
--- echoPostPHP.php
+++ echoPostPHP.php
@@ -1,13 +1,13 @@
 <?php
-header('Content-Type: text/html');
-echo "<html><head><title>Form Echo</title>";
-$color=$_POST["cSelect"];
-echo "</head><body bgcolor=$color><h1>";
-$first=$_POST["firstname"];
-$last=$_POST["lastname"];
-$dtime= date("Y-m-d", time());
-$theTime= time();
-echo "Welcome $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
-echo "</h1>";
-echo"</body></html>";
+	header('Content-Type: text/html');
+	echo "<html><head><title>Form Echo</title>";
+	$color=$_POST["color"];
+	echo "</head><body bgcolor=$color><h1>";
+	$first=$_POST["firstname"];
+	$last=$_POST["lastname"];
+	$dtime= date("Y-m-d", time());
+	$theTime= time();
+	echo "Welcome $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
+	echo "</h1>";
+	echo"</body></html>";
 ?>
