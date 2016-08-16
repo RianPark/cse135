8f53b08c0b9cb822f1decf41f0eb008048919d64
Jong Hwi Park <Jong Hwi Park>
2016-08-16 11:37:13 +0900

    2nd

--- echoGetPHP.php
+++ echoGetPHP.php
@@ -1,14 +1,12 @@
-
-
 <?php
-header('Content-Type: text/html');
-echo "<html><head><title>Form Echo</title>";
-$color=$_GET["color_select"];
-echo "</head><body bgcolor=$color><h1>";
-$first=$_GET["firstname"];
-$last=$_GET["lastname"];
+	header('Content-Type: text/html');
+	echo "<html><head><title>Form Echo</title>";
+	$color=$_GET["color_select"];
+	echo "</head><body bgcolor=$color><h1>";
+	$first=$_GET["firstname"];
+	$last=$_GET["lastname"];
 
-echo "Hello $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
-echo "</h1>";
-echo"</body></html>";
+	echo "<p>Hello $first $last from a Web app written in PHP </p>";
+	echo "<p> on .date("Y-m-d h:i:sa") ";
+	echo "</p></h1></body></html>";
 ?>
--- echoPostPHP.php
+++ echoPostPHP.php
@@ -5,8 +5,7 @@ $color=$_POST["color_select"];
 echo "</head><body bgcolor=$color><h1>";
 $first=$_POST["firstname"];
 $last=$_POST["lastname"];
-$dtime= date("Y-m-d", time());
-$theTime= time();
+
 echo "Hello $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
 echo "</h1>";
 echo"</body></html>";
