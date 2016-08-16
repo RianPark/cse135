08ca1ab3ef186ee0c87f819815d0f45ab518af14
Jong Hwi Park <Jong Hwi Park>
2016-08-16 10:22:26 +0900

    2nd

--- Enviro.php
+++ Enviro.php
@@ -1,21 +1,7 @@
 <?php
-$b = array(1, 1, 2, 3, 5, 8);
-
-$arr = get_defined_vars();
-
-// print $b
-print_r($arr["b"]);
-
-/* print path to the PHP interpreter (if used as a CGI)
- * e.g. /usr/local/bin/php */
-echo $arr["_"];
-
-// print the command-line parameters if any
-print_r($arr["argv"]);
-
-// print all the server vars
-print_r($arr["_SERVER"]);
-
-// print all the available keys for the arrays of variables
-print_r(array_keys(get_defined_vars()));
+$everything = get_defined_vars();
+ksort($everything);
+echo '<pre>';
+print_r($everything);
+echo '</pre>';
 ?>
