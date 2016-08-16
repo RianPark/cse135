3cc218baf5a0f4a3ecb03e3ab7ea43b8d868cd8c
Jong Hwi Park <Jong Hwi Park>
2016-08-16 10:17:34 +0900

    2nd

--- Enviro.php
+++ Enviro.php
@@ -1,9 +1,21 @@
 <?php
-	header('Content-Type: text/html'); 
-	echo "<html><head><title>Environment Variables</title>";
-	echo "</head><body>";
-	echo "<h1>Environment Variables : PHP</h1>";
-	echo "<pre>";
-	print_r($_ENV);
-	echo"</body></html>";
+$b = array(1, 1, 2, 3, 5, 8);
+
+$arr = get_defined_vars();
+
+// print $b
+print_r($arr["b"]);
+
+/* print path to the PHP interpreter (if used as a CGI)
+ * e.g. /usr/local/bin/php */
+echo $arr["_"];
+
+// print the command-line parameters if any
+print_r($arr["argv"]);
+
+// print all the server vars
+print_r($arr["_SERVER"]);
+
+// print all the available keys for the arrays of variables
+print_r(array_keys(get_defined_vars()));
 ?>
