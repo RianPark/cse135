ceba65fcf8d95eec943bb8e5d73b486e4926302f
Jong Hwi Park <Jong Hwi Park>
2016-08-16 09:17:18 +0900

    2nd

--- test2.js
+++ test2.js
@@ -3,13 +3,12 @@ var fs = require('fs');
 
 http.createServer(function (request, response) {
 
-	fs.readFile('/blueJS.html', (err, data) => {
+	fs.readFile('blueJS.html', (err, data) => {
 		console.log(data);
 	});
 
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   response.end('Hello World\n');
 }).listen(8000);
 
 // Console will print the message
