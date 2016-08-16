9f76cb89e0f7bd625825fc9e1e21020ef5062d90
Jong Hwi Park <Jong Hwi Park>
2016-08-16 09:01:41 +0900

    2nd

--- test2.js
+++ test2.js
@@ -9,11 +9,9 @@ app.listen(8000, function() {
 app.get('/', function( req, res) {
 
 	fs.readFile('blueJS.html', function(error, data) {
-		if(error){
-			console.log(error);
-		} else {
+		
 			res.writeHead(200,{'Content-Type': 'text/html'});
 			res.end(data);
-		}
+		
 	});
 });
