dbf573c4bdd867e4211153f380551a48264e9db1
Jong Hwi Park <Jong Hwi Park>
2016-08-16 11:48:29 +0900

    2nd

--- echoPostPHP.php
+++ echoPostPHP.php
@@ -1,12 +1,11 @@
 <?php
-	header('Content-Type: text/html');
-	echo "<html><head><title>Form Echo</title>";
 	$color=$_POST["color_select"];
-	echo "</head><body bgcolor=$color><h1>";
 	$first=$_POST["firstname"];
 	$last=$_POST["lastname"];
+	header('Content-Type: text/html');
 	
-	echo "Hello $first $last from a Web app written in PHP on ".date("Y-m-d h:i:sa");
-	echo "</h1>";
-	echo"</body></html>";
+	echo "<html><head><title>Form Echo</title>";
+	echo "</head><body bgcolor=$color><h1>";
+	echo "Hello $first $last, Web app with PHP, post method on ".date("Y-m-d h:i:sa");
+	echo "</h1></body></html>";
 ?>
